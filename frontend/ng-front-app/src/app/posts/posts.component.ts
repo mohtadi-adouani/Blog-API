2dcb311881c739a67853b50285ce15adaf1ae00e
Mohtadi <mohtadi.adouani@gmail.com>
2024-12-25 15:10:53 +0100

    - modify posts

--- frontend/ng-front-app/src/app/posts/posts.component.ts
+++ frontend/ng-front-app/src/app/posts/posts.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { Post} from '../post';
+import {UpperCasePipe} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-posts',
-  imports: [],
+  imports: [
+    UpperCasePipe,
+    FormsModule
+  ],
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.css'
 })
 export class PostsComponent {
-  title = 'Windstorm';
+  post: Post = {
+    id: 1,
+    title: "Exemple de titre",
+    body: "Exemple de body",
+  };
 
 }
